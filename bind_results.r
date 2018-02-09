38216c1c99e41e5a98ffee1ca7a3415bb014697f
Peter Shen <peter.shen@aimsio.com>
2018-02-09 15:19:04 -0500

    binding files together in the end

---
+++ bind_results.r
@@ -0,0 +1,6 @@
+gwaResult <- data.frame(`rsid`=character(), `MinorAllele`=character(),  `MajorAllele`=character(), `DiseaseFrequency`=numeric(), `ControlFrequency`=numeric(), `OR`=numeric(), `pvalue`=numeric(), `hwpvalue`=numeric(), stringsAsFactors=F)
+for(i in 1:22) {
+  gwaResult <- bind_rows(gwaResult, read.delim(paste0("./results/gwa", i, ".csv"), sep="\t", header=T))
+}
+
+write.table(gwaResult, file=paste0("gwa_results.csv"), sep="\t")
