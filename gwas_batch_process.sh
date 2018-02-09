f34bf0837dab9ce7716b0cf69169d9620544b262
Peter Shen <peter.shen@aimsio.com>
2018-02-09 14:27:34 -0500

    fix sequencing

--- gwas_batch_process.sh
+++ gwas_batch_process.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-for i in {1..2}
+for i in $(seq -f "%02g" 1 22)
 do
-    sbatch -p short -n 6 -t 0-2:00 --mem=2G --job-name gwas-analysis -o %j.out -e %j.err --mail-type=ALL --mail-user=Peter_Shen@hms.harvard.edu --wrap="sh gwas_single_chr $i"
+    sbatch -p short -n 6 -t 0-2:00 --mem=2G --job-name gwas-analysis -o %j.out -e %j.err --mail-type=ALL --mail-user=Peter_Shen@hms.harvard.edu --wrap="./gwas_single_chr.sh $i"
     sleep 1 # wait 1 second between each job submission
 done
 exit
