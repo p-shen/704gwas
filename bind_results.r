63b229a1548a70e05d4fb607c7647f4b470da9bf
Peter Shen <peter.shen@aimsio.com>
2018-02-10 12:39:50 -0500

    rework gwas using dplyr

--- bind_results.r
+++ bind_results.r
@@ -1,3 +1,4 @@
+library(tidyverse)
 gwaResult <- data.frame(`rsid`=character(), `MinorAllele`=character(),  `MajorAllele`=character(), `DiseaseFrequency`=numeric(), `ControlFrequency`=numeric(), `OR`=numeric(), `pvalue`=numeric(), `hwpvalue`=numeric(), stringsAsFactors=F)
 for(i in 1:22) {
   gwaResult <- bind_rows(gwaResult, read.delim(paste0("./results/gwa", i, ".csv"), sep="\t", header=T))
