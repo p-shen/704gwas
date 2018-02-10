2f53f9888a89155a47d25e48dc6995d8bc846b07
Peter Shen <peter.shen@aimsio.com>
2018-02-10 14:13:19 -0500

    add some debug information

--- gwas_batch_process.sh
+++ gwas_batch_process.sh
@@ -2,7 +2,7 @@
 
 for i in $(seq -f "%02g" 1 22)
 do
-    sbatch -p short -n 6 -t 0-8:00 --mem=16G --job-name gwas-analysis -o %j.out -e %j.err --mail-type=ALL --mail-user=Peter_Shen@hms.harvard.edu --wrap="./gwas_single_chr.sh $i"
+    sbatch -p short -n 6 -t 0-8:00 --mem=16G --job-name gwa-$i -o %j.out -e %j.err --mail-type=ALL --mail-user=Peter_Shen@hms.harvard.edu --wrap="./gwas_single_chr.sh $i"
     sleep 0.5 # wait 1 second between each job submission
 done
 exit
