f687fc9d173b18c86578c3532c16edaa0424b519
Peter Shen <peter.shen@aimsio.com>
2018-02-11 00:09:47 -0500

    binding changes

--- bind_results.r
+++ bind_results.r
@@ -1,7 +1,10 @@
 library(tidyverse)
-gwaResult <- data.frame(`rsid`=character(), `MinorAllele`=character(),  `MajorAllele`=character(), `DiseaseFrequency`=numeric(), `ControlFrequency`=numeric(), `OR`=numeric(), `pvalue`=numeric(), `hwpvalue`=numeric(), stringsAsFactors=F)
-for(i in 1:22) {
-  gwaResult <- bind_rows(gwaResult, read.delim(paste0("./results/gwa", i, ".csv"), sep="\t", header=T))
-}
+
+gwaResult <- do.call(rbind, lapply(1:22, function(i) {
+  read.delim(paste0("./results/gwa", i, ".csv"), sep="\t", header=T)
+}))
+# for(i in 1:22) {
+#   gwaResult <- bind_rows(gwaResult, read.delim(paste0("./results/gwa", i, ".csv"), sep="\t", header=T))
+# }
 
 write.table(gwaResult, file=paste0("gwa_results.csv"), sep="\t")
