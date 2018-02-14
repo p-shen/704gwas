3476fee2ead14b091521a80d67f8070bad79abb7
Peter Shen <peter.shen@aimsio.com>
2018-02-14 13:53:25 -0500

    completed analysis

--- bind_results.r
+++ bind_results.r
@@ -1,7 +1,14 @@
 library(tidyverse)
 
-gwaResult <- do.call(rbind, lapply(1:22, function(i) {
-  read.delim(paste0("./results/gwa", i, ".csv"), sep="\t", header=T)
-}))
+diseases <- c("BD", "CAD", "CD", "HT", "RA", "T1D", "T2D")
 
-write.table(gwaResult, file=paste0("gwa_results.csv"), sep="\t")
+gwaResult.All <- do.call(rbind, lapply(diseases, function(disease) {
+    gwaResult <- do.call(rbind, lapply(1:22, function(i) {
+      read.delim(paste0(disease,"/gwa", i, ".csv"), sep="\t", header=T)
+    }))
+    gwaResult$Disease <- disease
+    return(gwaResult)
+  })
+)
+
+write.table(gwaResult.All, file=paste0("gwa_results_all.csv"), sep="\t")
--- sandbox.R
+++ sandbox.R
@@ -1,12 +1,4 @@
 
-gwa5.imm <- read.delim('./imm_bk/gwa5.csv', sep="\t", header=T)
-gwa5.res <- read.delim('./results/gwa5.csv', sep="\t", header=T)
-snps <- read.delim('imm_bk/snps/snps_22', sep="\t", header=F)
-
-gwa22.imm <- read.delim('./imm_bk/gwa22.csv', sep="\t", header=T)
-gwa22.res <- read.delim('./results/gwa22.csv', sep="\t", header=T)
-
-
 test.table <- bind_rows(apply(control[1:5,-1:-4], 1, function(x){
   as.list(table(t(x)))
 }))
@@ -27,14 +19,3 @@ test.table.counts <- bind_cols(test.table, bind_rows(apply(test.table, 1, functi
   
   as.list(c(`MajorAllele`=majorAllele, `MinorAllele`=minorAllele, `MajorAlleleCount`=majorAlleleCount, `MinorAlleleCount`=minorAlleleCount))
 })))
-
-print(append(uniqchars, ret))
-
-snpTable.colNames <- unlist(strsplit(colnames(snpTable[,2]), " "))
-snpTable.additive <- snpTable %>% transmute(a1=.[[1]]*2+.[[2]], a2=.[[2]]+.[[3]]*2)
-colnames(snpTable.additive) <- snpTable.colNames
-
-# determine major and minor alleles
-majorAllele <- max.col(snpTable.additive[1,])
-minorAllele <- colnames(snpTable.additive[1,-majorAllele])
-majorAllele <- colnames(snpTable.additive[1,majorAllele])
